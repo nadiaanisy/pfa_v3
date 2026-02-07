0b62cc3d2e85154ee85d330d2ac001a98d84dcc9
Isyqi Saiful <saif@sitecore.net>
2026-02-08 04:51:08 +0800

    (Components) Routes: Protected Route

---
+++ src/components/routes/ProtectedRoute.tsx
@@ -0,0 +1,27 @@
+import React, { JSX } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../miscellaneous/Providers';
+
+interface ProtectedRouteProps {
+  children: JSX.Element;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+    <div className="flex h-screen items-center justify-center">
+      <p>Loading...</p>
+    </div>
+  );
+  }
+
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
